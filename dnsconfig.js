049f89fed89cdb9fb3c03818ba72707306c46299
Major Hayden <major@mhtx.net>
2024-07-23 12:25:59 -0500

    Switch to porkbun

--- dnsconfig.js
+++ dnsconfig.js
@@ -3,92 +3,61 @@ DEFAULTS(
 );
 
 var REG_NONE = NewRegistrar("none");
-var DSP_CLOUDFLARE = NewDnsProvider("cloudflare", { "manage_redirects": true });
+var DSP_PORKBUN = NewDnsProvider("porkbun");
 
-// SPF records for domains without email.
-var SPF_NO_MX = [
-    TXT("@", "v=spf1 -all")
-]
+D("major.io", REG_NONE, DnsProvider(DSP_PORKBUN),
+	DefaultTTL(600),
 
-// Fastmail config.
-var FASTMAIL_RECORDS = [
-    MX('@', 10, 'in1-smtp.messagingengine.com.'),
-    MX('@', 20, 'in2-smtp.messagingengine.com.'),
-    MX('*', 10, 'in1-smtp.messagingengine.com.'),
-    MX('*', 20, 'in2-smtp.messagingengine.com.'),
-]
-var FASTMAIL_DKIM = function (the_domain) {
-    return [
-        CNAME('fm1._domainkey', 'fm1.' + the_domain + '.dkim.fmhosted.com.'),
-        CNAME('fm2._domainkey', 'fm2.' + the_domain + '.dkim.fmhosted.com.'),
-        CNAME('fm3._domainkey', 'fm3.' + the_domain + '.dkim.fmhosted.com.')
-    ]
-}
+	// Redirects
+	ALIAS("www", "uixie.porkbun.com."),
 
-D("major.io", REG_NONE, DnsProvider(DSP_CLOUDFLARE),
-    ALIAS("@", "major-io.pages.dev.", CF_PROXY_ON),
-    TXT('_e6e00522b262580d125eda419b2bf2a3', '_6241c4cde2128f9cc4cf436bc4d69f12.kdbplsmznr.acm-validations.aws'),
-    TXT('_github-pages-challenge-major', '5ac40cc1b766ec2929e0de23035406'),
-    TXT('_github-pages-challenge-major.t4r', '54cd4b87c8c881dd40743807d4745e'),
-    CNAME('origin', 'major.github.io.', CF_PROXY_ON),
-    CNAME('stats', 'major.github.io.', CF_PROXY_ON),
-    TXT('_dmarc', 'v=DMARC1; p=reject; rua=mailto:d978cefa8ddd442a83ed8515c20646db@dmarc-reports.cloudflare.net;'),
-    TXT("@", "v=spf1 include:spf.messagingengine.com ~all"),
-    TXT("@", "google-site-verification=tCM6cfzWPV6wZ2RZ9aFzZe1dl2TBGjYXjdTz1K8WUNU"),
-    FASTMAIL_RECORDS,
-    FASTMAIL_DKIM("major.io")
-);
+	ALIAS("@", "dppzfofnt5y9n.cloudfront.net."),
+	ALIAS("stats", "major.github.io."),
+	ALIAS("txlf24-tech-career", "major.github.io."),
+	ALIAS("txlf24-containers", "major.github.io."),
 
-D("mhtx.net", REG_NONE, DnsProvider(DSP_CLOUDFLARE),
-    A("@", "103.168.172.37", CF_PROXY_ON),
-    A("@", "103.168.172.52", CF_PROXY_ON),
-    A("*", "103.168.172.37", CF_PROXY_ON),
-    A("*", "103.168.172.52", CF_PROXY_ON),
-    A("coreos", "5.161.211.180"),
-    A("hetzner", "5.161.210.54"),
-    CNAME("firewall", "he708y0f0sr.sn.mynetname.net."),
-    TXT("default._bimi", "v=BIMI1;l=https://mhtx.net/cowboy-hat-face.svg;a="),
-    TXT('_dmarc', 'v=DMARC1;  p=none; rua=mailto:5de49ed820ef403faf1ca9b3593f6e9e@dmarc-reports.cloudflare.net'),
-    // SPF records to include Fedora mailing lists
-    TXT('@', 'v=spf1 include:_spfcf1.mhtx.net include:spf.messagingengine.com -all'),
-    TXT('_spfcf1', 'v=spf1 ip4:38.145.60.11 ip4:38.145.60.12  -all'),
-    // postmaster.google.com verification
-    TXT("@", "google-site-verification=q1rSmvpqOMTZ2dczLgHVb5qqWtgOFYIFcwMgPHEQaps"),
-    FASTMAIL_RECORDS,
-    FASTMAIL_DKIM("mhtx.net")
-);
+	// Mail
+	CNAME("status", "page.updown.io."),
+	CNAME("fm1._domainkey", "fm1.major.io.dkim.fmhosted.com."),
+	CNAME("fm2._domainkey", "fm2.major.io.dkim.fmhosted.com."),
+	CNAME("fm3._domainkey", "fm3.major.io.dkim.fmhosted.com."),
+	MX("@", 10, "in1-smtp.messagingengine.com."),
+	MX("@", 20, "in2-smtp.messagingengine.com."),
+	MX("*", 10, "in1-smtp.messagingengine.com."),
+	MX("*", 20, "in2-smtp.messagingengine.com."),
+	TXT("@", "v=spf1 include:spf.messagingengine.com ?all"),
 
-// D("thetanerd.com", REG_NONE, DnsProvider(DSP_CLOUDFLARE),
-//     // Ghost Pro Blog
-//     A("@", "5.161.127.225", CF_PROXY_ON),
-//     A("www", "5.161.127.225", CF_PROXY_ON),
-//     // Old Hugo CloudFlare Pages blog
-//     CNAME("old", "thetanerd-com.pages.dev.", CF_PROXY_ON),
-//     TXT("@", "v=spf1 include:spf.messagingengine.com ~all"),
-//     TXT("@", "google-site-verification=mycVLMWJTjw_mHgmbZNISWAhPKpzBozyQpOHArhkUAY"),
-//     TXT("_dmarc", "v=DMARC1; p=none"),
-//     // Mailgun
-//     TXT("smtp._domainkey.posts", "k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAt02XptCGz6qdE2r5m9hh4RIWZqzVyTCym1gWsQ8uD0xKNmXNlkmk6eSg3ZkHDCbQwTPrUR8RFzFPe2NglDO7euwM2bSpppGEnigzUlIKtXTgpTZzVKTOXghixFkMtIrlJxSlgMacZVhUvx6siWPs1uS8exk2D6TXoWDISr6PN5AADdG518uVCjsvhvF0RSPPW7u4Bx6P1FP73Cuk/3unOxsXQ1PFHzzJ/YxfWHJGUfMGkkVmbeGpdDK0Tq05xy9TBlvHsYRyWO277jx32uoKQsiSQ3FFl25DdjWrO+v8mBO92dz4X8i3RgPXx+aK+3yK9Z1ZbHn43AY/7I6FdyzPBQIDAQAB"),
-//     TXT("posts", "v=spf1 include:mailgun.org ~all"),
-//     MX("posts", 10, "mxa.mailgun.org."),
-//     MX("posts", 10, "mxb.mailgun.org."),
-//     CNAME("email.posts", "mailgun.org."),
-//     // Fastmail
-//     MX('@', 10, 'in1-smtp.messagingengine.com.'),
-//     MX('@', 20, 'in2-smtp.messagingengine.com.'),
-//     FASTMAIL_DKIM("thetanerd.com")
-// );
+	TXT("@", "google-site-verification=tCM6cfzWPV6wZ2RZ9aFzZe1dl2TBGjYXjdTz1K8WUNU"),
+	TXT("_updown.status", "updown-page=uy5w"),
+	TXT("_github-pages-challenge-major", "5ac40cc1b766ec2929e0de23035406"),
+END);
 
-// D("tootchute.com", REG_NONE, DnsProvider(DSP_CLOUDFLARE),
-//     A("@", "5.161.181.52"),
-//     // A("*", "5.161.181.52"),
-//     CNAME("tootfiles", "f001.backblazeb2.com.", CF_PROXY_ON),
-//     SPF_NO_MX
-// );
+D("mhtx.net", REG_NONE, DnsProvider(DSP_PORKBUN),
+	DefaultTTL(600),
+	A("hv", "162.213.193.18"),
 
-D("w5wut.com", REG_NONE, DnsProvider(DSP_CLOUDFLARE),
-    A("@", "1.2.3.4", CF_PROXY_ON),
-    A("*", "1.2.3.4", CF_PROXY_ON),
-    CF_REDIRECT("*w5wut.com/*", "https://major.io/w5wut/"),
-    SPF_NO_MX
-);
+	// Redirects
+	ALIAS("@", "uixie.porkbun.com."),
+	CNAME("*", "uixie.porkbun.com."),
+
+	// Mail
+	CNAME("firewall", "he708y0f0sr.sn.mynetname.net."),
+	CNAME("fm1._domainkey", "fm1.mhtx.net.dkim.fmhosted.com."),
+	CNAME("fm2._domainkey", "fm2.mhtx.net.dkim.fmhosted.com."),
+	CNAME("fm3._domainkey", "fm3.mhtx.net.dkim.fmhosted.com."),
+	CNAME("mesmtp._domainkey", "mesmtp.mhtx.net.dkim.fmhosted.com."),
+	MX("@", 120, "mailsec.protonmail.ch."),
+	MX("@", 10, "in1-smtp.messagingengine.com."),
+	MX("@", 20, "in2-smtp.messagingengine.com."),
+	MX("@", 110, "mail.protonmail.ch."),
+	TXT("@", "v=spf1 include:spf.messagingengine.com -all"),
+	TXT("_dmarc", "v=DMARC1; p=quarantine"),
+END);
+
+D("w5wut.com", REG_NONE, DnsProvider(DSP_PORKBUN),
+	DefaultTTL(600),
+	ALIAS("@", "uixie.porkbun.com."),
+	CNAME("*", "uixie.porkbun.com."),
+	ALIAS("qrz", "uixie.porkbun.com."),
+	ALIAS("fcc", "uixie.porkbun.com."),
+END);
